3748798f3883668f05ffbfbccdfd2e76dc9351eb
Boris Schapira <boris@schapira.dev>
2025-03-24 18:00:45 +0100

    Custom fork
    - Reformat code for my own use
    - Update sha1.js to 0.7.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # FavIdenticon
 
-In the Chrome Web Store at https://chrome.google.com/webstore/detail/favidenticon-for-google-d/alkonpgilhahbbhihdjnamkmckfajmjo
+My own local version of the extension available in the Chrome Web Store at https://chrome.google.com/webstore/detail/favidenticon-for-google-d/alkonpgilhahbbhihdjnamkmckfajmjo
 
-![](icons/screen640.png "")
+![](icons/screen640.png)
--- index.js
+++ index.js
@@ -1,12 +1,77 @@
-var docId = document.URL.match(/\/d\/([_A-Za-z0-9-]+)/)[1];
-var docHash = sha1(docId);
-var bgColor;
-switch (document.URL.match(/docs.google.com\/(\w+)/)[1]) {
-	case 'document': bgColor = [81, 142, 245, 255]; break;
-	case 'drawings': bgColor = [222, 83, 71, 255]; break;
-	case 'forms': bgColor = [114, 73, 188, 255]; break;
-	case 'presentation': bgColor = [245, 186, 19, 255]; break;
-	case 'spreadsheets': bgColor = [35, 165, 102, 255];
+class Config {
+  constructor(name, idRegex, bgColor, fgColor) {
+    this.name = name;
+    this.idRegex = idRegex;
+    this.bgColor = bgColor;
+    this.fgColor = fgColor;
+  }
+
+  matches(url) {
+    return url.match(this.idRegex);
+  }
+
+  docId(url) {
+    return this.matches(url)[1];
+  }
 }
-var iconBase64 = new Identicon(docHash, { background: bgColor, foreground: [255, 255, 255, 255] }).toString();
-document.querySelector('link[rel*="icon"]').href = 'data:image/png;base64,' + iconBase64;
+
+let configs = [
+  new Config(
+    "Google Docs",
+    /docs.google.com\/document\/d\/([_A-Za-z0-9-]+)/,
+    [15, 105, 255, 255],
+    [255, 255, 255, 255],
+  ),
+  new Config(
+    "Google Drawings",
+    /docs.google.com\/drawings\/d\/([_A-Za-z0-9-]+)/,
+    [222, 83, 71, 255],
+    [255, 255, 255, 255],
+  ),
+  new Config(
+    "Google Forms",
+    /docs.google.com\/forms\/d\/([_A-Za-z0-9-]+)/,
+    [114, 73, 188, 255],
+    [255, 255, 255, 255],
+  ),
+  new Config(
+    "Google Slides",
+    /docs.google.com\/presentation\/d\/([_A-Za-z0-9-]+)/,
+    [245, 186, 19, 255],
+    [74, 74, 74, 255],
+  ),
+  new Config(
+    "Google Sheets",
+    /docs.google.com\/spreadsheets\/d\/([_A-Za-z0-9-]+)/,
+    [29, 135, 83, 255],
+    [255, 255, 255, 255],
+  ),
+  new Config(
+    "Google Drive File",
+    /drive.google.com\/file\/d\/([_A-Za-z0-9-]+)/,
+    [180, 0, 224, 255],
+    [255, 255, 255, 255],
+  ),
+  new Config(
+    "Google Drive Folder",
+    /drive.google.com\/drive\/folders\/([_A-Za-z0-9-]+)/,
+    [180, 0, 224, 255],
+    [255, 255, 255, 255],
+  ),
+];
+
+// Find a config that matches the current URL
+let config = configs.find((config) => config.matches(document.URL));
+
+// If there is a config for the current URL, update the favicon based on that config
+if (config) {
+  let docId = config.docId(document.URL);
+  let docHash = sha1(docId);
+
+  let iconBase64 = new Identicon(docHash, {
+    background: config.bgColor,
+    foreground: config.fgColor,
+  }).toString();
+  document.querySelector('link[rel*="icon"]').href =
+    "data:image/png;base64," + iconBase64;
+}
